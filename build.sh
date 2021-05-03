f90fe179eae65bf8aff7a6649717e21af71d7c20
Romaric Philogene <evoxmusic@gmail.com>
2021-05-03 21:27:41 +0200

    fix: github actions workflow

--- build.sh
+++ build.sh
@@ -1,37 +1,15 @@
 #!/usr/bin/env sh
 
-# MacOSX dependencies:
-# 1. Install Xcode
-# 2. Install Rust - https://www.rust-lang.org/tools/install
-# 3. Run `brew install mingw-w64`
-# 4 Run `brew install FiloSottile/musl-cross/musl-cross`
-# 5 Run `brew tap SergioBenitez/osxct && brew install x86_64-unknown-linux-gnu`
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  # Linux
-  echo "Platform Linux detected"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  # Mac OSX
-  echo "Platform MacOSX detected"
-elif [[ "$OSTYPE" == "win32" ]]; then
-  # Windows
-  echo "Platform Windows detected"
-else
-  echo "Platform not supported"
-  exit 1
-fi
-
 rustup target add x86_64-pc-windows-gnu
-rustup target add x86_64-unknown-linux-gnu
 rustup target add x86_64-apple-darwin
 
 # Let's build
 cd redisless
-cargo clean
 
+cargo clean
+cargo build --release
 RUSTFLAGS="-C linker=x86_64-apple-darwin14-clang" cargo build --target x86_64-apple-darwin --release
 RUSTFLAGS="-C linker=x86_64-w64-mingw32-gcc" cargo build --target x86_64-pc-windows-gnu --release
-RUSTFLAGS="-C linker=x86_64-unknown-linux-gnu-gcc" cargo build --target x86_64-unknown-linux-gnu --release
 
 cd ..
 
@@ -54,10 +32,10 @@ cp redisless/target/x86_64-pc-windows-gnu/release/redisless.dll clients/nodejs/l
 cp redisless/target/x86_64-pc-windows-gnu/release/redisless.dll clients/golang/lib/libredisless.dll
 
 # Linux Python
-cp redisless/target/x86_64-unknown-linux-gnu/release/libredisless.so clients/python/src/libredisless.so
+cp redisless/target/release/libredisless.so clients/python/src/libredisless.so
 
 # Linux NodeJS
-cp redisless/target/x86_64-unknown-linux-gnu/release/libredisless.so clients/nodejs/lib/libredisless.so
+cp redisless/target/release/libredisless.so clients/nodejs/lib/libredisless.so
 
 # Linux Golang
-cp redisless/target/x86_64-unknown-linux-gnu/release/libredisless.so clients/golang/lib/libredisless.so
+cp redisless/target/release/libredisless.so clients/golang/lib/libredisless.so
