d9f99c55836ae16983086108ac62e58db0f89fa9
clarity <marcosmejias@mailfence.com>
2021-05-08 19:26:06 +0800

    remove setex storage method

--- redisless/storage/src/lib.rs
+++ redisless/storage/src/lib.rs
@@ -3,7 +3,6 @@ pub mod in_memory;
 pub trait Storage {
     fn write(&mut self, key: &[u8], value: &[u8]);
     fn expire(&mut self, key: &[u8], duration: u64) -> u32;
-    // fn setex(&mut self, key: &[u8], value: &[u8], duration: u64);
     fn read(&mut self, key: &[u8]) -> Option<&[u8]>;
     fn remove(&mut self, key: &[u8]) -> u32;
 }
