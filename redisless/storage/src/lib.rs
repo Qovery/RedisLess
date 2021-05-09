920ef7e9a0e3ea7214906d8cd1ac108b6c4b467e
clarity <marcosmejias@mailfence.com>
2021-05-10 02:36:00 +0800

    change expire to take Expiry instead of a u64

--- redisless/storage/src/lib.rs
+++ redisless/storage/src/lib.rs
@@ -1,8 +1,10 @@
+use in_memory::Expiry;
+
 pub mod in_memory;
 
 pub trait Storage {
     fn write(&mut self, key: &[u8], value: &[u8]);
-    fn expire(&mut self, key: &[u8], duration: u64) -> u32;
+    fn expire(&mut self, key: &[u8], expiry: Expiry) -> u32;
     fn read(&mut self, key: &[u8]) -> Option<&[u8]>;
     fn remove(&mut self, key: &[u8]) -> u32;
 }
